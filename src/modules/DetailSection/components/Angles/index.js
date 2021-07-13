594cb428c800bc1bdc718e6a07bbb77145064f63
gauravs0113 <87378940+gauravs0113@users.noreply.github.com>
2021-07-13 20:04:18 +0530

    Add files via upload

---
+++ src/modules/DetailSection/components/Angles/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ImageCheckList from '../ImageCheckList'
+
+const Angles = () => {
+    return <ImageCheckList type="angle" title="Angles Required" />
+}
+
+export default Angles
